41dceff1a16875e8287917cea716c65b153ba567
roi <roibuenaventura05@gmail.com>
2025-08-22 20:51:47 +0800

    handle error in prod

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
 import { db } from './db';
 import { users } from './schema';
-import { eq } from 'drizzle-orm';
+
+const fetchdata = async () => {
+  try {
+    const allUsers = await db.select().from(users);
+    return { data: allUsers, error: false };
+  } catch (error) {
+    console.log(error);
+    return { data: null, error: true };
+  }
+};
 
 export default async function Home() {
-  const allUsers = await db.select().from(users);
+  const { data, error } = await fetchdata();
+
+  if (error) {
+    <h1 className='text-9xl'>There is an error</h1>;
+  }
 
   return (
     <div className='h-screen flex items-center justify-center'>
       <h1 className='text-9xl'>My first website in cloudflare</h1>
       <div>
-        {allUsers.map(user => {
+        {data?.map(user => {
           return (
             <div key={user.id}>
               <h1>{user.name}</h1>
